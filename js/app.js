fd567ef80364a928fec973c49fba01c707c1fe57
pinguno <r4f4hc8@gmail.com>
2022-10-18 10:03:24 +0200

    Actualizacion JS

--- js/app.js
+++ js/app.js
@@ -1,43 +1,46 @@
+function validarLogin(){
+    const email = document.getElementById('email');
+    const pass = document.getElementById('password');
+    const form = document.getElementById('form');
+    const parrafo = document.getElementById('warnings');
+
+    form.addEventListener("submit", e =>{
+        e.preventDefault()
+        let warnings = ""
+        let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/
+        let entrar= false
+        let nIntentos = 0
+        parrafo.innerHTML = ""
+        
+        if(!regexEmail.test(email.value)){
+
+            warnings += 'El email no es valido <br>'
+            entrar = true
+
+        }
+
+        if(pass.value.length < 4){
+        
+            warnings += 'La contraseña no es valida <br>'
+            entrar = true
+
+        }
+
+        
+
+        if(entrar){
+            parrafo.innerHTML = warnings
+        }else{
+            parrafo.innerHTML = "enviado"
+        }
+
+        
 
 
-const email = document.getElementById('email');
-const pass = document.getElementById('password');
-const form = document.getElementById('form');
-const parrafo = document.getElementById('warnings');
+    })
 
-form.addEventListener("submit", e =>{
-    e.preventDefault()
-    let warnings = ""
-    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/
-    let entrar= false
-    let nIntentos = 0
-    parrafo.innerHTML = ""
-    
-    if(!regexEmail.test(email.value)){
-
-        warnings += 'El email no es valido <br>'
-        entrar = true
-
-    }
-
-    if(pass.value.length < 4){
-    
-        warnings += 'La contraseña no es valida <br>'
-        entrar = true
-
-    }
-
-    
-
-    if(entrar){
-        parrafo.innerHTML = warnings
-    }else{
-        parrafo.innerHTML = "enviado"
-    }
-
-    
+}
 
 
-})
 
 
